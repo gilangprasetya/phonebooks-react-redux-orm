35dc31765f77d481711c865b1f0c19b0c9485b00
Gilang pRasetya <gilangprasetya9@gmail.com>
2023-08-02 10:49:47 +0700

    DONE REDUX

--- REACT-REDUX/src/index.js
+++ REACT-REDUX/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client"; // Import createRoot from react-dom/client
 
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/phonebooks";
 import App from "./App";
